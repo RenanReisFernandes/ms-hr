9afaaef83c63d3d828137c4fd6877288b72d5c17
Renan Reis <renanmacfernandes@hotmail.com>
2024-07-14 01:16:40 -0300

    criacao de metodo de atualizar Workers

--- hr-worker/src/main/java/com/renan/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/renan/hrworker/controllers/WorkerController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +40,18 @@ public class WorkerController {
 	public ResponseEntity<Optional<Worker>> findById(@PathVariable Long id){
 		Optional<Worker> worker = service.findById(id);
 		return ResponseEntity.status(HttpStatus.FOUND).body(worker);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Worker> update(@RequestBody Worker workerUpdated, @PathVariable Long id){
+		try {
+		Worker worker = service.update(workerUpdated, id);
+		return ResponseEntity.status(HttpStatus.OK).body(worker);
+		}
+		catch (Exception e) {
+			return ResponseEntity.noContent().build();
+		}
+		
 	}
 
 }
