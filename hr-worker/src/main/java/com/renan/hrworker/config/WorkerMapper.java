6651834410b773a0578698a0c1cfad38227e3246
Renan Reis <renanmacfernandes@hotmail.com>
2024-07-14 02:23:08 -0300

    criacao da classe WorkerMapper

---
+++ hr-worker/src/main/java/com/renan/hrworker/config/WorkerMapper.java
@@ -0,0 +1,11 @@
+package com.renan.hrworker.config;
+
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class WorkerMapper {
+
+}
