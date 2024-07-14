f3bd3e90f3db333167719a84a45675028215a76e
Renan Reis <renanmacfernandes@hotmail.com>
2024-07-14 01:00:19 -0300

    criacao de metodo de encontrar por id Workers

--- hr-worker/src/main/java/com/renan/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/renan/hrworker/controllers/WorkerController.java
@@ -1,11 +1,13 @@
 package com.renan.hrworker.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +33,12 @@ public class WorkerController {
 	public ResponseEntity<List<Worker>> findAll(){
 		List<Worker> list = service.findAll();
 		return ResponseEntity.status(HttpStatus.FOUND).body(list);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Optional<Worker>> findById(@PathVariable Long id){
+		Optional<Worker> worker = service.findById(id);
+		return ResponseEntity.status(HttpStatus.FOUND).body(worker);
 	}
 
 }
