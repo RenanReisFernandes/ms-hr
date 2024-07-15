cf5feb4518751c5bf49005f32880ce54fb763a76
Renan Reis <renanmacfernandes@hotmail.com>
2024-07-15 00:42:20 -0300

    Adicao do swagger em pagamentos

--- hr-payroll/src/main/java/com/renan/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/renan/hrpayroll/HrPayrollApplication.java
@@ -3,7 +3,11 @@ package com.renan.hrpayroll;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "API PAYROLL", version = "1.0.0", description = "API de controle de pagamentos"))
 public class HrPayrollApplication {
 
 	public static void main(String[] args) {
--- hr-payroll/src/main/java/com/renan/hrpayroll/controller/PaymentController.java
+++ hr-payroll/src/main/java/com/renan/hrpayroll/controller/PaymentController.java
@@ -11,11 +11,25 @@ import org.springframework.web.bind.annotation.RestController;
 import com.renan.hrpayroll.entities.Payment;
 import com.renan.hrpayroll.paymentService.PaymentService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 @RestController
 @RequestMapping(value = "/payments")
 public class PaymentController {
 	@Autowired
 	private PaymentService service;
+	
+	@Operation(summary = "Realiza chamada do valor do pagamento pelo funcionário/dias/valor da diária", method = "GET")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Busca valores de pagamentos"),
+			@ApiResponse(responseCode = "422", description = "Dados de requisição inválidos"),
+			@ApiResponse(responseCode = "400", description = "Parâmetros inválidos"),
+			@ApiResponse(responseCode = "401", description = "Usuário não encontrado"),
+			@ApiResponse(responseCode = "500", description = "Erro de servidor")
+			
+	})
 	
 	@GetMapping(value = "/{workerId}/dias/{dias}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer dias){
