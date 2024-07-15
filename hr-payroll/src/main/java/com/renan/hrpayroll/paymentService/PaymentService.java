0ef1852813932405e7c5dcc7a418a5a1613fbc98
Renan Reis <renanmacfernandes@hotmail.com>
2024-07-15 13:07:08 -0300

    Implementacao do RestTemplate

--- hr-payroll/src/main/java/com/renan/hrpayroll/paymentService/PaymentService.java
+++ hr-payroll/src/main/java/com/renan/hrpayroll/paymentService/PaymentService.java
@@ -1,14 +1,31 @@
 package com.renan.hrpayroll.paymentService;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.renan.hrpayroll.entities.Payment;
+import com.renan.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 	
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	public Payment getPayment(long workerId, int dias) {
-		return new Payment("Renan", 20000.00,dias);
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id",""+ workerId);
+		
+		Worker worker = restTemplate.getForObject(workerHost + "/worker/{id}", Worker.class, uriVariables);
+		return new Payment(worker.getName(),worker.getDailyIncome(),dias);
 	}
 
 }
